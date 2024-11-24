4a317100a0f7491d8797ad4ea5564cc56b93d29f
level09 <level09@gmail.com>
2024-11-24 20:02:16 +0100

    pump version to 0.1.4

--- src/neuronic/__init__.py
+++ src/neuronic/__init__.py
@@ -1,6 +1,6 @@
 """Neuronic - AI-powered data transformation library"""
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .core import Neuronic, OutputType, NeuronicError, APIKeyError, TransformationError
 
